564ca538935bc6849465cb0eb0af3ce27ea496c4
matutaj <matutajorge2002@gmail.com>
2023-03-24 10:10:13 +0100

    operação ternária

---
+++ OperacaoTernario.js
@@ -0,0 +1,13 @@
+const valorUsuario = 999;
+//Ver se o usuario tem uma area de  usuário normal ou privado
+const situacaousuario =
+  valorUsuario >= 1000 ? "Usuário Privado" : "Usário Normal!";
+
+console.log(situacaousuario);
+
+//condição normal
+/*
+if (valorUsuario >= 1000) {
+  console.log("Usuario Privado");
+} else console.log("usuário Normal!");
+*/
