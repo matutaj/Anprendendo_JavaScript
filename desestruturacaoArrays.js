d2b01030206b20a52da5569452de68a9aca91576
matutaj <matutajorge2002@gmail.com>
2023-04-06 11:23:17 +0100

    manipulando com arrays compostas

--- desestruturacaoArrays.js
+++ desestruturacaoArrays.js
@@ -9,7 +9,7 @@ const letras = [2,4, 5];
 [a, b, c]= letras;
 
 console.log(a, b, c)
-*/
+
 
 const numeros = [1, 2, 3, 4, 5 ,6, 7, 8, 9];
 
@@ -17,3 +17,12 @@ const [primeiroN, SegundoN, ...resto]= numeros;
 //...resto = > restOperat 
 console.log(primeiroN, SegundoN);
 console.log(resto)
+*/
+
+const numeros = [[1, 3, 4], [2, 5, 6],[7, 8, 9] ];
+const [lista1, lista2, lista3] = numeros;
+const [, [, , seis]] = numeros;
+console.log(lista1)
+console.log(seis)
+
+
