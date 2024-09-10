0dc666c527a81e51d89acd33840df458407451bf
matutaj <matutajorge2002@gmail.com>
2024-09-10 11:07:38 +0100

    Trabalhando em DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,8 @@
 "use strict";
 
 const h2 = document.querySelector("h2");
-console.log(h2);
+const classP = document.querySelector(".paragrafo strong");
+const idP = document.querySelector("#paragrafo");
+console.log(idP)
+console.log(classP);
+h2.style.background = "red"; 
