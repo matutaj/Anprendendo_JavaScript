9eded1eb99c6ae0110a46e2fd2331ff16cb3faed
matutaj <matutajorge2002@gmail.com>
2024-09-10 11:17:45 +0100

    manipulando elementos html no js

--- DOM/script.js
+++ DOM/script.js
@@ -3,6 +3,18 @@
 const h2 = document.querySelector("h2");
 const classP = document.querySelector(".paragrafo strong");
 const idP = document.querySelector("#paragrafo");
-console.log(idP)
-console.log(classP);
+
 h2.style.background = "red"; 
+
+const p = document.querySelectorAll("p");
+
+p.forEach((element, index)=>{
+    element.style.color ="#fff";
+    element.style.padding= "20px"
+        if( index === 0){
+            element.style.background = "blue";
+        }
+        if (index ===1){
+            element.style.background = "red";
+        }
+})
