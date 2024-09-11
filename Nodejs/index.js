9b054ec49191c9e9161d7edde5d6253170e31659
matutaj <matutajorge2002@gmail.com>
2024-09-11 05:58:44 +0100

    add servidor

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,1 +1,27 @@
-console.log(3000);
+import http from "node:http";
+
+const server = http.createServer ((req, res)=>{
+
+    const {url, method} = req;
+    
+    if(url === "/" && method === "GET"){
+        return res.end(
+            JSON.stringify({
+                status:200,
+                body:"Bem Vindo(a) a Home!"
+            })
+        );
+    }
+
+    res.statusCode =404;
+    return res.end(
+        JSON.stringify({
+            status:404,
+            body:"404 (Not found )",
+        })
+    );
+});
+
+server.listen(3000, ()=>{
+    console.log("Hello World!")
+})
