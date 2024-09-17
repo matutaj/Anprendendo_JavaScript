7c9fb481e27144493388030a8858ff673921034e
matutaj <matutajorge2002@gmail.com>
2024-09-17 10:23:16 +0100

    feat: add

---
+++ DOM/textContent_innertext/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// textContent => escreve os textos com formatação mas não escreve elementos HTML
+//  innertext => escreve os textos sem formatação e sem elemente HTML
+// innerHTML => Escreve textos com formatação com  e sem elementos HTml
+
+//const p = document.querySelector("p");
+
+//console.log(p.textContent);
+//console.log(p.innerText);
+
+//p.textContent = "Pegando melhor o conceito de DOM"
+
+const div = document.querySelector("div");
+
+//console.log(1, div);
+
+//div.innerHTML = `${div.innerHTML} <strong> Esse é o texo alterado </strong>`
+
+// criar elemento 
+const elementUl = document.createElement("ul");
+console.log(elementUl)
+const numberArray = [1,2,3];
+// addicionando elemento dentro da Ul
+numberArray.forEach((element) => {
+    const elementLi = document.createElement("li");
+    elementUl.appendChild(elementLi);
+});
+div.appendChild(elementUl);
