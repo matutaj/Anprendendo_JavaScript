1063d5be7782f5f28a896d3d2e1904c59dd5cbf0
matutaj <matutajorge2002@gmail.com>
2024-09-23 10:18:00 +0100

    feat: add classList

---
+++ DOM/encontrar elemento/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const strong = document.querySelector("strong");
+const container = document.querySelector(".container")
+// pegando o elemento pai
+console.log(strong.parentElement);
+console.log(strong.parentNode)
+
+//pegando o elemento filho
+console.log(container.children);
+console.log(container.childNodes);
